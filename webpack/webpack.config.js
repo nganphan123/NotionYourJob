644007005c67041782da76811a78e842a88b0e50
nganphan123 <jill01009@gmail.com>
2024-05-09 23:27:55 -0700

    update webpack

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const CopyPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
   mode: "production",
   entry: {
@@ -19,9 +20,20 @@ module.exports = {
         loader: "ts-loader",
         exclude: /node_modules/,
       },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
     ],
   },
   plugins: [
+    new HtmlWebpackPlugin({
+      title: "notionjobs", // change this to your app title
+      manifest: "manifest.json",
+      filename: "popup.html",
+      template: path.resolve(__dirname, "..", "src", "popup.html"),
+      hash: true,
+    }),
     new CopyPlugin({
       patterns: [{ from: ".", to: ".", context: "public" }],
     }),
