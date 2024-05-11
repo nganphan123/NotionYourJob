7aa340263f0d539236b7413406767da516fbbff8
nganphan123 <jill01009@gmail.com>
2024-05-11 03:15:53 -0700

    remove parsing in webpack

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -6,7 +6,6 @@ module.exports = {
   entry: {
     background: path.resolve(__dirname, "..", "src", "background.ts"),
     popup: path.resolve(__dirname, "..", "src", "popup.ts"),
-    parsing: path.resolve(__dirname, "..", "src", "parsing.ts"),
   },
   output: {
     path: path.join(__dirname, "../dist"),
