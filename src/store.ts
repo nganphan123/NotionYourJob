4f73db26378ba7be674518e1ee8e35f21fc82487
nganphan123 <jill01009@gmail.com>
2024-05-13 14:56:21 -0700

    fix store

--- src/store.ts
+++ src/store.ts
@@ -1,5 +1,6 @@
+// Turn a function to Promise
 const toPromise = (callback: any) => {
-  const promise = new Promise((resolve, reject) => {
+  const promise = new Promise<string>((resolve, reject) => {
     try {
       callback(resolve, reject);
     } catch (err) {
@@ -11,42 +12,46 @@ const toPromise = (callback: any) => {
 
 const DB_ID_KEY = "dbId";
 
+// Jobs database Id
 export function getDBId() {
   return toPromise((resolve: any, reject: any) => {
     chrome.storage.local.get([DB_ID_KEY], (result) => {
       if (chrome.runtime.lastError) reject(chrome.runtime.lastError);
-      resolve(result ?? "");
+      resolve(result[DB_ID_KEY] ?? "");
     });
   });
 }
 
 export async function setDBId(id: string) {
   return toPromise((resolve: any, reject: any) => {
-    chrome.storage.local.set({ [DB_ID_KEY]: id }),
-      () => {
-        if (chrome.runtime.lastError) reject(chrome.runtime.lastError);
-        resolve("Store db id successfully");
-      };
+    chrome.storage.local.set({ [DB_ID_KEY]: id }, () => {
+      if (chrome.runtime.lastError) {
+        reject(chrome.runtime.lastError);
+      }
+      resolve("Store db id successfully");
+    });
   });
 }
 
+// Description Page Id
 const DESC_PAGE_ID = "descPageId";
 export function getDescPageId() {
   return toPromise((resolve: any, reject: any) => {
     chrome.storage.local.get([DESC_PAGE_ID], (result) => {
       if (chrome.runtime.lastError) reject(chrome.runtime.lastError);
-      resolve(result ?? "");
+      resolve(result[DESC_PAGE_ID] ?? "");
     });
   });
 }
 
 export async function setDescPageId(id: string) {
   return toPromise((resolve: any, reject: any) => {
-    chrome.storage.local.set({ [DESC_PAGE_ID]: id }),
-      () => {
-        if (chrome.runtime.lastError) reject(chrome.runtime.lastError);
-        resolve("Store desc page id successfully");
-      };
+    chrome.storage.local.set({ [DESC_PAGE_ID]: id }, () => {
+      if (chrome.runtime.lastError) {
+        reject(chrome.runtime.lastError);
+      }
+      resolve("Store desc page id successfully");
+    });
   });
 }
 
@@ -54,18 +59,21 @@ const ACCESS_TOKEN = "accessToken";
 export function getAcessToken() {
   return toPromise((resolve: any, reject: any) => {
     chrome.storage.local.get([ACCESS_TOKEN], (result) => {
-      if (chrome.runtime.lastError) reject(chrome.runtime.lastError);
-      resolve(result ?? "");
+      if (chrome.runtime.lastError) {
+        reject(chrome.runtime.lastError);
+      }
+      resolve(result[ACCESS_TOKEN] ?? "");
     });
   });
 }
 
-export async function setAccessToken(id: string) {
+export async function setAccessToken(token: string) {
   return toPromise((resolve: any, reject: any) => {
-    chrome.storage.local.set({ [ACCESS_TOKEN]: id }),
-      () => {
-        if (chrome.runtime.lastError) reject(chrome.runtime.lastError);
-        resolve("Store access token successfully");
-      };
+    chrome.storage.local.set({ [ACCESS_TOKEN]: token }, () => {
+      if (chrome.runtime.lastError) {
+        reject(chrome.runtime.lastError);
+      }
+      resolve("Store access token successfully");
+    });
   });
 }
