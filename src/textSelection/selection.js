8150a84c8e49052f674f8a0749f916e01b2c37f4
nganphan123 <ngan01009@gmail.com>
2024-10-20 00:59:14 -0700

    add on text selection handler

--- src/textSelection/selection.js
+++ src/textSelection/selection.js
@@ -1,5 +1,4 @@
 // TODO: fix file into typescript
-import { MessageType } from "../background";
 import "./selection.css";
 // Unique ID for the className.
 var MOUSE_VISITED_CLASSNAME = "crx_mouse_visited";
@@ -7,6 +6,12 @@ var MOUSE_VISITED_CLASSNAME = "crx_mouse_visited";
 // Previous dom, that we want to track, so we can remove the previous styling.
 var prevDOM = null;
 
+const handleTxtSelected = function (e) {
+  chrome.runtime.sendMessage({
+    type: MessageType,
+  });
+  console.log("selected text" + e.target.innerText);
+};
 // Mouse listener for any move event on the current document.
 document.addEventListener(
   "mousemove",
@@ -15,19 +20,16 @@ document.addEventListener(
 
     if (prevDOM != null) {
       prevDOM.classList.remove(MOUSE_VISITED_CLASSNAME);
+      prevDOM.removeEventListener("click", handleTxtSelected);
     }
 
     // Add a visited class name to the element. So we can style it.
     if (srcElement != null) {
       srcElement.classList.add(MOUSE_VISITED_CLASSNAME);
-      // srcElement.addEventListener('click',function(e){
-      //   chrome.runtime.sendMessage({
-      //     type:MessageType
-      //   })
-      // })
+      srcElement.addEventListener("click", handleTxtSelected);
 
-      // The current element is now the previous. So we can remove the class
-      // during the next iteration.
+      // The current element is now the previous.
+      // So we can remove the class during the next iteration.
       prevDOM = srcElement;
     }
   },
