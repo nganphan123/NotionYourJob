61cb473c9b9fcd60ef7f22060c6a8f2a7f7abe48
nganphan123 <ngan01009@gmail.com>
2024-11-22 01:38:01 -0800

    fix auth flow

--- src/components/RequireAuth.tsx
+++ src/components/RequireAuth.tsx
@@ -1,12 +1,13 @@
 import Button from "@mui/material/Button";
-import { isUserLogin, login } from "../auth";
+import { isUserLogin } from "../auth";
 import { ReactElement, useEffect, useState } from "react";
 import { MessageType } from "../background";
 import Stack from "@mui/material/Stack";
-import { Container } from "@mui/material";
+import { CircularProgress, Container } from "@mui/material";
 
 export default function RequireAuth({ children }: { children: ReactElement }) {
   const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+  const [loading, setLoading] = useState<boolean>(false);
 
   useEffect(() => {
     isUserLogin().then((res) => {
@@ -16,6 +17,13 @@ export default function RequireAuth({ children }: { children: ReactElement }) {
 
   if (isLoggedIn) {
     return children;
+  } else if (loading) {
+    return (
+      <Stack alignItems={"center"} gap={2}>
+        <div>Logging in ...</div>
+        <CircularProgress color="inherit" />
+      </Stack>
+    );
   } else {
     return (
       <Stack direction={"column"} alignItems={"center"}>
@@ -29,9 +37,10 @@ export default function RequireAuth({ children }: { children: ReactElement }) {
             margin: "10px",
           }}
           variant="contained"
-          onClick={async () =>
-            await chrome.runtime.sendMessage({ type: MessageType.LOG_IN })
-          }
+          onClick={async () => {
+            await chrome.runtime.sendMessage({ type: MessageType.LOG_IN });
+            setLoading(true);
+          }}
         >
           Log in
         </Button>
